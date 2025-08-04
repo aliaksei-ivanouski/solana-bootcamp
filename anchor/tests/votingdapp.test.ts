1f7c4766040d27af16db45a0ab70a13b49f5cfbe
aliaksei.ivanouski <aliaksei.ivanouski@gmail.com>
2025-08-04 18:52:54 +0400

    main
    - add all tests in the folder;

--- anchor/tests/votingdapp.test.ts
+++ anchor/tests/votingdapp.test.ts
@@ -1,4 +1,3 @@
-import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
 import { PublicKey } from '@solana/web3.js';
 import { Votingdapp } from '../target/types/votingdapp';
