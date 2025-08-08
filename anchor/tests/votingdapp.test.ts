b6c2c04177cf563185102910a5435ab1299dce04
aliaksei.ivanouski <aliaksei.ivanouski@gmail.com>
2025-08-08 17:40:18 +0400

    main
    - remove hard code params in test and add workspace one;

--- anchor/tests/votingdapp.test.ts
+++ anchor/tests/votingdapp.test.ts
@@ -2,51 +2,37 @@ import anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
 import { PublicKey } from '@solana/web3.js';
 import { Votingdapp } from '../target/types/votingdapp';
-import { ProgramTestContext, startAnchor } from 'solana-bankrun';
-import { BankrunProvider } from 'anchor-bankrun';
 import { describe, it, expect, beforeAll } from 'vitest';
-import IDL from '../target/idl/votingdapp.json' assert { type: 'json' };
 import BN from 'bn.js';
-
-const votingdappAddress = new PublicKey("3jMZPXYMF3Mk3PvbPpaHhkARax3PQ7DAbYhq2SY5kM7L")
 
 describe('votingdapp', () => {
 
-  let context: ProgramTestContext;
-  let provider: BankrunProvider;
-  // const provider = anchor.AnchorProvider.env();
-  // anchor.setProvider(provider);
+  const pollId = new BN(Date.now());
   let program: Program<Votingdapp>;
 
   beforeAll(async () => {
-    context = await startAnchor("./anchor", [{
-      name: "votingdapp",
-      programId: votingdappAddress
-    }], []);
-    provider = new BankrunProvider(context);
-    program = new Program<Votingdapp>(
-      IDL,
-      provider,
-    );
+    process.chdir('./anchor');
+    anchor.setProvider(anchor.AnchorProvider.env());
+    program = anchor.workspace.Votingdapp as Program<Votingdapp>;
   })
 
   it('Initialize Poll', async () => {
     await program.methods.initializePoll(
-      new BN(1),
+      pollId,
       "What is your favourite pinut butter?",
       new BN(0),
       new BN(1854222157),
     ).rpc();
 
     const [pollAddress] = PublicKey.findProgramAddressSync(
-      [new BN(1).toArrayLike(Buffer, 'le', 8)],
-      votingdappAddress
+      [pollId.toArrayLike(Buffer, 'le', 8)],
+      program.programId
     );
 
     const poll = await program.account.poll.fetch(pollAddress);
     console.log(poll);
 
-    expect(poll.pollId.toNumber()).toBe(1);
+    expect(poll.pollId.toNumber()).toBe(pollId.toNumber());
     expect(poll.description).toBe("What is your favourite pinut butter?");
     expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
     expect(poll.pollStart.toNumber()).toBe(0);
@@ -57,18 +43,18 @@ describe('votingdapp', () => {
   it('Initialize Candidate', async () => {
     await program.methods.initializeCandidate(
       "Crunchy",
-      new BN(1),
+      pollId,
     ).rpc();
 
     await program.methods.initializeCandidate(
       "Smooth",
-      new BN(1),
+      pollId,
     ).rpc();
 
 
     const [CrunchyAddress] = PublicKey.findProgramAddressSync(
-      [new BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Crunchy")],
-      votingdappAddress
+      [pollId.toArrayLike(Buffer, 'le', 8), Buffer.from("Crunchy")],
+      program.programId
     );
 
     const Crunchy = await program.account.candidate.fetch(CrunchyAddress);
@@ -78,8 +64,8 @@ describe('votingdapp', () => {
     expect(Crunchy.candidateVotes.toNumber()).toBe(0);
 
     const [SmoothAddress] = PublicKey.findProgramAddressSync(
-      [new BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Smooth")],
-      votingdappAddress
+      [pollId.toArrayLike(Buffer, 'le', 8), Buffer.from("Smooth")],
+      program.programId
     );
 
     const Smooth = await program.account.candidate.fetch(SmoothAddress);
@@ -94,20 +80,20 @@ describe('votingdapp', () => {
   it('Vote', async () => {
     await program.methods.vote(
       "Crunchy",
-      new BN(1),
+      pollId,
     ).rpc();
 
     const [CrunchyAddress] = PublicKey.findProgramAddressSync(
-      [new BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Crunchy")],
-      votingdappAddress
+      [pollId.toArrayLike(Buffer, 'le', 8), Buffer.from("Crunchy")],
+      program.programId
     );
 
     const Crunchy = await program.account.candidate.fetch(CrunchyAddress);
     console.log(Crunchy);
 
     const [SmoothAddress] = PublicKey.findProgramAddressSync(
-      [new BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Smooth")],
-      votingdappAddress
+      [pollId.toArrayLike(Buffer, 'le', 8), Buffer.from("Smooth")],
+      program.programId
     );
 
     const Smooth = await program.account.candidate.fetch(SmoothAddress);
